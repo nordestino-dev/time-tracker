9116e5254a6ee85ab3695f2a5f5a06a9d80dbc95
CodeSandbox <support@codesandbox.io>
2025-03-02 19:20:37 +0000

    Sandbox snapshot commit

---
+++ user-workspace/time-tracker/src/components/Register.tsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
